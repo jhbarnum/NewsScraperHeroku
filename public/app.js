490c4444d196cd5b3afea13d1ffc2fa32c134702
Jared <jhbarnum@gmail.com>
2018-04-27 17:12:37 -0400

    adding ScreenShots

--- public/app.js
+++ public/app.js
@@ -116,7 +116,7 @@ $.getJSON("/saved", function (data) {
   
   // For each one
   for (var i = 0; i < data.length; i++) {
-
+  //for (var i = data.length; i > 0; i--) {
     // Display the apropos information on the page
     $("#saved").append("<p data-id='" + data[i]._id + "'>" +
       data[i].title + "<br />" +
